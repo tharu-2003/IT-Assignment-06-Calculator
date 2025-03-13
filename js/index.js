c28b090ac4adcd6fa731a4941457cb153a2fb12a
tharu-2003 <sandaruwantharusha968@gmail.com>
2025-03-13 00:00:05 -0700

    file added

--- js/index.js
+++ js/index.js
@@ -1,11 +1,40 @@
+let oparator = null ;
+let firstNumber = null ;
+let lastNumber = null ;
+let hafExpression = null ;
+
 function clearDisplay(){
-    document.getElementById('input-text').value = ' ';
+    document.getElementById('input-text').value = null;
+    this.oparator = null ;
+    this.firstNumber = null ;
+    this.lastNumber = null ;
+    this.hafExpression = null ;
 }
 function calculate(){
-    let expression = document.getElementById('input-text').value;
+    this.lastNumber = document.getElementById('input-text').value;
+
+    let expression = `${this.hafExpression } ${this.lastNumber}`;
+
     let result = eval(expression);
     document.getElementById('input-text').value = result;
+
+    this.oparator = null ;
+    this.firstNumber = null ;
+    this.lastNumber = null ;
+    this.hafExpression = null ;
 }
-function appendNumber(number) {
+function appendNumber(number){
     document.getElementById('input-text').value += number;
+}
+function appendOparator(oparator){
+
+    this.firstNumber = document.getElementById('input-text').value;
+    this.oparator = oparator;
+    
+    if( this.hafExpression == null){
+        this.hafExpression = `${this.firstNumber } ${this.oparator}`;
+    }else{
+        this.hafExpression += `${this.firstNumber } ${this.oparator}`;
+    }
+    document.getElementById('input-text').value = null;
 }
